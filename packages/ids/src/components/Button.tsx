2ccf342cc5169360ed1c2e51fd1a5cb0bf25d0d1
Mikko Forsström <mikko.forsstrom@veikkaus.fi>
2024-09-22 10:18:07 +0300

    Luls lus luu

--- packages/ids/src/components/Button.tsx
+++ packages/ids/src/components/Button.tsx
@@ -3,13 +3,13 @@ import { ComponentProps, FC } from "react";
 import * as stylex from "@stylexjs/stylex";
 import { colors } from "@stylexjs/open-props/lib/colors.stylex";
 import { sizes } from "@stylexjs/open-props/lib/sizes.stylex";
-import { borders } from "@stylexjs/open-props/lib/borders.stylex";
+// import { borders } from "@stylexjs/open-props/lib/borders.stylex";
 
 const styles = stylex.create({
   button: {
-    borderRadius: `${borders.radius3}`,
+    borderRadius: `var(--radius-2)`,
+    borderWidth: `var(--border-size-1)`,
     borderStyle: "solid",
-    borderWidth: borders.size1,
     borderColor: `light-dark(${colors.gray9}, ${colors.gray8})`,
     backgroundColor: `light-dark(${colors.gray5}, ${colors.gray7})`,
     paddingBlock: sizes.spacing2,
--- packages/ids/src/components/Input.tsx
+++ packages/ids/src/components/Input.tsx
@@ -3,15 +3,16 @@ import { ComponentProps, FC } from "react";
 import * as stylex from "@stylexjs/stylex";
 import { colors } from "@stylexjs/open-props/lib/colors.stylex";
 import { sizes } from "@stylexjs/open-props/lib/sizes.stylex";
-import { borders } from "@stylexjs/open-props/lib/borders.stylex";
+// import { borders } from "@stylexjs/open-props/lib/borders.stylex";
 
 const styles = stylex.create({
   input: {
+    borderRadius: `var(--radius-2)`,
+    borderWidth: `var(--border-size-1)`,
+
     backgroundColor: `light-dark(${colors.gray5}, ${colors.gray7})`,
     color: `light-dark(${colors.gray12}, ${colors.gray3})`,
-    borderRadius: borders.radius3,
     borderStyle: "solid",
-    borderWidth: `${borders.size1}`,
     borderColor: `light-dark(${colors.gray9}, ${colors.gray8})`,
     paddingBlock: sizes.spacing2,
     paddingInline: sizes.spacing3,
