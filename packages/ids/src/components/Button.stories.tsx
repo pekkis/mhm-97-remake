f58b70076b15771728b0eeb331a2a50b419341dc
Mikko Forsström <mikko.forsstrom@veikkaus.fi>
2024-09-22 09:47:18 +0300

    Hehe haha hoho

--- packages/ids/src/components/Button.stories.tsx
+++
@@ -1,8 +0,0 @@
-import type { StoryDefault, Story } from "@ladle/react";
-import { Button } from "./Button";
-
-export default {
-  title: "Button",
-} satisfies StoryDefault;
-
-export const Default: Story = () => <Button>Hip hurraa sanoi peksu</Button>;
