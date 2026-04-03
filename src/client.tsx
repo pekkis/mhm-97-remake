40aee240dfe8d9bc797144e3d8835d1a25d6b898
Mikko Forsström <mikko.forsstrom@nitor.com>
2026-04-03 18:54:13 +0300

    Start of refactor.

--- src/client.tsx
+++ src/client.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client"
 import Root from "./Root";
 
 import { getInitialState } from "./config/state";
@@ -13,16 +13,16 @@ import {
   faExclamationCircle
 } from "@fortawesome/free-solid-svg-icons";
 
-import runtime from "@dr-kobros/serviceworker-webpack-plugin/lib/runtime";
-
 import "./style.pcss";
 
-import * as Sentry from "@sentry/browser";
+// import * as Sentry from "@sentry/browser";
 
+/*
 Sentry.init({
   environment: process.env.NODE_ENV,
   dsn: "https://38630b0d78f645abb5cae7e82bc1582c@sentry.io/1367033"
 });
+*/
 
 library.add(faSpinner, faBars, faExclamationCircle);
 
@@ -32,7 +32,10 @@ const store = createStore(initialState);
 
 // Just a small DRY abstraction here.
 function render(Component: typeof Root, rootElement: HTMLElement) {
-  ReactDOM.render(<Component store={store} />, rootElement);
+
+  const root = createRoot(rootElement)
+
+  root.render(<Component store={store} />);
 }
 
 // If we get !undefined state from the server, we hydrate.
@@ -43,15 +46,3 @@ if (!rootElement) {
 
 render(Root, rootElement);
 
-runtime.register().then((sw: ServiceWorker) => {
-  console.log(sw, "serviis wörker");
-});
-
-// Webpack's hot reloading magic happens here.
-
-if (module.hot) {
-  module.hot.accept("./Root", () => {
-    const HotReloadedRoot = require("./Root").default;
-    render(HotReloadedRoot, rootElement);
-  });
-}
